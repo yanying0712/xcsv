047bb2a8b8a5a083b6f5ac2984acbdbce6128fb7
Ja Long <longja703@outlook.com>
2019-06-14 18:03:21 +0800

    repair the issue of BTCMarkets

--- xcsv.js
+++ xcsv.js
@@ -2,6 +2,38 @@ var DeltaOutput = "Date,Type,Exchange,Base amount,Base currency,Quote amount,Quo
 
 var exchangeData = {};
 
+var returnRow = function(exchange,data) {
+    var result = [];
+    for(var Column in DeltaOutput) {
+
+        if( exchange[ DeltaOutput[Column] ] !== undefined) {
+            // console.log(exchange[DeltaOutput[Column]]);
+            var convertedData = exchange[ DeltaOutput[Column] ](data);
+
+            if(convertedData !== undefined)
+                result[Column] = convertedData;
+            else
+                result[Column] = "";
+        } else {
+            console.warn("Column " + DeltaOutput[Column] + " Not managed");
+        }
+    }
+    return result;
+};
+
+var pushRow = function(results, temp_result){
+    if(!(temp_result[3] == 0))
+        if(!((temp_result[5] == 0 && temp_result[6]) || temp_result[4] == 'STR'))
+            if(temp_result[0] !== 'Invalid date')
+                if(temp_result[4].length <= 5)
+                    if(temp_result[5] !== 'Addition')
+                        results.push(temp_result.join(","));
+
+
+
+    return results;
+};
+
 (function( $ ){
 
     var methods = {
@@ -119,31 +151,117 @@ var exchangeData = {};
 
             var results = [];
 
-            for(var data in csvdata) {
-                result = [];
-
-                for(var Column in DeltaOutput) {
-
-                    if( exchange[ DeltaOutput[Column] ] !== undefined) {
-                        var convertedData = exchange[ DeltaOutput[Column] ]( csvdata[data] );
-
-                        if(convertedData !== undefined)
-                            result[Column] = convertedData;
-                        else
-                            result[Column] = "";
-                    } else {
-                        console.warn("Column " + DeltaOutput[Column] + " Not managed");
+            if(pExchange === 'BTCMarkets'){
+                console.log(csvdata.length);
+                var pause = false;
+                var pause_step = 0;
+                var temp_result = [];
+                function returnDate(data){
+                    var date = moment(data['creationTime'], "YYYY-MM-DD, hh:mm aa");
+                    return date.utc().format("YYYY-MM-DD hh:mm:ss Z");
+                }
+                function returnAmount(data){
+                    var amount = data['amount'];
+                    return Math.abs(amount.replace(/,/g, ''));
+                }
+                function returnType(data) {
+                    var type = data['action'];
+                    switch(type){
+                        case 'Deposit':
+                            return 'DEPOSIT';
+                        case 'Withdraw':
+                            return 'WITHDRAW';
+                        case 'Buy Order':
+                            return 'BUY';
+                        case 'Sell Order':
+                            return 'SELL';
+                        case 'Trading Fee':
+                            return 'BUY';
+                        default:
+                            return '';
                     }
                 }
-            // it will be not pushed if the "Base Amount" and "Quote Amount" of result equal "0";
-                if(!(result[3] == 0))
-                    if(!((result[5] == 0 && result[6]) || result[4] == 'STR'))
-                        if(result[0] !== 'Invalid date')
-                            if(result[4].length <= 5)
-                                if(result[5] !== 'Addition')
-                                    results.push(result.join(","));
+                for(var i in csvdata){
+                    if(csvdata[i]['recordType'] === 'Fund Transfer'){
+                        pause = false;
+                        pause_step = 0;
+                    }else{
+                        pause = true;
+                    }
+
+                    if(pause === false){
+                        temp_result = returnRow(exchange,csvdata[i]);
+                        results = pushRow(results,temp_result);
+                        temp_result = [];
+                    }
+                    else if(pause == true){
+                        console.log('pause:',pause);
+                        console.log('pause_step:',pause_step);
+
+                        var action = csvdata[i]['action'];
+
+                        console.log('action:',action);
+
+                        switch (pause_step) {
+                            case 0:
+                                temp_result[0] = returnDate(csvdata[i]);
+                                console.log('date:',temp_result);
+                                temp_result[1] = returnType(csvdata[i]);
+                                temp_result[2] = 'BTCMarkets';
+                                if(action === 'Buy Order'){
+                                    temp_result[5] = returnAmount(csvdata[i]);
+                                    temp_result[6] = csvdata[i]['currency'];
+                                }else{
+                                    temp_result[3] = returnAmount(csvdata[i]);
+                                    temp_result[4] = csvdata[i]['currency'];
+                                }
+                                temp_result[9] = temp_result[10] = temp_result[12] = temp_result[13] = '';
+                                temp_result[11] = '1';
+                                temp_result[14] = csvdata[i]['description'].replace(/\r?\n|\r|,/g, ' ');
+                                break;
+                            case 1:
+                                if(action === 'Buy Order'){
+                                    temp_result[3] = returnAmount(csvdata[i]);
+                                    temp_result[4] = csvdata[i]['currency'];
+                                }else{
+                                    temp_result[5] = returnAmount(csvdata[i]);
+                                    temp_result[6] = csvdata[i]['currency'];
+                                }
+                                break;
+                            case 2:
+                                temp_result[7] = returnAmount(csvdata[i]);
+                                temp_result[8] = csvdata[i]['currency'];
+                                console.log("temp 3:",JSON.stringify(results));
+
+                                if(!(temp_result[3] == 0))
+                                    if(!((temp_result[5] == 0 && temp_result[6]) || temp_result[4] == 'STR'))
+                                        if(temp_result[0] !== 'Invalid date')
+                                            if(temp_result[4].length <= 5)
+                                                if(temp_result[5] !== 'Addition')
+                                                    results.push(temp_result.join(","));
+                                temp_result = [];
+                                break;
+                        }
+
+                        pause_step ++;
+                        if(pause_step === 3){
+                            pause_step = 0;
+                        }
+                    }
+
+
+                }
 
             }
+            else{
+                for(var i in csvdata) {
+                    result = [];
+                    result = returnRow(exchange,csvdata[i]);
+                    results = pushRow(results,result);
+                    result = [];
+                }
+            }
+
             return results.join('\n');
         }
     };
